f749074b3d1c722968788989cf23c5a5bbcf4253
Shohel Rana <shohelrrrana@gmail.com>
2023-02-17 16:31:30 +0600

    fix sending mail

--- src/api/v1/utils/sendEmail.ts
+++ src/api/v1/utils/sendEmail.ts
@@ -1,34 +1,27 @@
 import nodemailer, { SentMessageInfo } from 'nodemailer'
-import { OAuth2Client }                from 'google-auth-library'
 
 const CLIENT_ID     = process.env.GOOGLE_CLIENT_ID
 const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
-const REFRESH_TOKEN = process.env.GOOGLE_REFRESH_TOKEN
-const REDIRECT_URI  = 'https://developers.google.com/oauthplayground'
+const EMAIL_FROM    = process.env.EMAIL_FROM
+const EMAIL_USER    = process.env.EMAIL_USER
+const EMAIL_PASS    = process.env.EMAIL_PASS
 
-//make google auth
-const authClient = new OAuth2Client( CLIENT_ID, CLIENT_SECRET, REDIRECT_URI )
-authClient.setCredentials( { refresh_token: REFRESH_TOKEN } )
-
-const sendEmail = async ( to: string, subject: string, html: string ): Promise<SentMessageInfo> => {
+const sendEmail = async( to: string, subject: string, html: string ): Promise<SentMessageInfo> => {
     try {
-        const { token }   = await authClient.getAccessToken()
         const transporter = nodemailer.createTransport( {
             service: 'gmail',
             host: 'smtp.gmail.com',
             secure: true,
             auth: {
-                type: 'OAuth2',
-                user: process.env.EMAIL_USER,
+                user: EMAIL_USER,
+                pass: EMAIL_PASS,
                 clientId: CLIENT_ID,
-                clientSecret: CLIENT_SECRET,
-                refreshToken: REFRESH_TOKEN,
-                accessToken: token!
+                clientSecret: CLIENT_SECRET
             }
         } )
 
         const mailOptions = {
-            from: process.env.EMAIL_FROM,
+            from: EMAIL_FROM,
             to,
             subject,
             html
