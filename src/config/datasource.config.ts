0a9475de778872ef8e28e4705c4ad7434fb1eefd
Shohel Rana <shohelrrrana@gmail.com>
2023-05-24 01:20:14 +0600

    change db env

--- src/config/datasource.config.ts
+++ src/config/datasource.config.ts
@@ -1,23 +1,15 @@
 import path from 'path'
 import { DataSource } from 'typeorm'
 
-const dbConnection: any      = process.env.DB_CONNECTION || 'postgres'
-const host: string           = process.env.DB_HOST || '127.0.0.1'
-const port: number           = Number( process.env.DB_PORT ) || 3306
-const username: string       = process.env.DB_USERNAME!
-const password: string       = process.env.DB_PASSWORD!
-const database: string       = process.env.DB_DATABASE!
-const dbSynchronize: boolean = Boolean( process.env.DB_SYNCHRONIZE )
+const databaseType: any     = process.env.DATABASE_TYPE || 'postgres'
+const databaseUrl           = process.env.DATABASE_URL
+const databaseSync: boolean = Number( process.env.DATABASE_SYNC ) === 1
 
 export const appDataSource = new DataSource( {
-    type: dbConnection,
-    host: host,
-    port: port,
-    username: username,
-    password: password,
-    database: database,
-    ssl: { rejectUnauthorized:false },
-    synchronize: dbSynchronize,
+    type: databaseType,
+    url: databaseUrl,
+    ssl: { rejectUnauthorized: false },
+    synchronize: databaseSync,
     logging: false,
     migrationsRun: false,
     entities: [
